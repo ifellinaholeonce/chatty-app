58552b848a0daa8fd977ac71a83954896b5b21c8
Tymm Schmitke <ifellinaholeonce@gmail.com>
2018-01-09 22:15:50 +0000

    receiving broadcasts from server

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,29 @@ class App extends Component {
     super(props);
 
     this.state = {
-      socket: "ws://127.0.0.1:3001",
       currentUser: {name: "Bob"},
       messages: []
     };
   }
 
   componentDidMount = () => {
-    this.connection = new WebSocket(this.state.socket);
+    this.connection = new WebSocket("ws://127.0.0.1:3001");
     this.connection.onopen = () => {
       console.log("Connected to server as:", this.state.currentUser);
-      this.connection.send('Connected ' + this.state.currentUser);
     };
+    this.connection.onmessage = (event) => {
+      let message = JSON.parse(event.data);
+      const messages = this.state.messages.concat(message);
+      this.setState({messages});
+    }
 
   }
 
   newMessage = (e) => {
     if (e.key === "Enter") {
       let msg = e.target.value;
-      const newMessage = {username: this.state.currentUser.name, content: msg, id: (this.state.messages.length + 1)};
-      const messages = this.state.messages.concat(newMessage);
-      this.setState({messages : messages});
-      this.connection.send(this.state.currentUser.name + ' said ' + msg);
+      const newMessage = {username: this.state.currentUser.name, content: msg};
+      this.connection.send(JSON.stringify(newMessage));
       e.target.value = ""
     }
   }
